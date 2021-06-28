38c3cdca64f0d47cc7e97335ef5c1fa3f39bdbde
cannel14 <cannelmaina@gmail.com>
2021-06-28 14:01:56 -0700

    tasks functions

--- 0x05-pointers_arrays_strings/0-reset_to_98.c~
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c~
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include "holberton.h"
 /**
  * reset_to_98 - function tha takes a pointer as parameter and updates to 98
  *int - parameter and n variable
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include "holberton.h"
 /**
  *rev_string - function that reverses a string
  * @char *s: character pointer to s string
@@ -6,21 +6,21 @@
  */
 void rev_string(char *s)
 {
-  int len, i, half;
-	char temp;
+int len, i, half;
+char temp;
 
-	for (len = 0; s[len] != '\0'; len++)
-		;
+for (len = 0; s[len] != '\0'; len++)
+;
 
-	i = 0;
+i = 0;
 
-	half = len / 2;
+half = len / 2;
 
-	while (half--)
-	{
-		temp = s[len - i - 1];
-		s[len - i - 1] = s[i];
-		s[i] = temp;
-		i++;
-	}
+while (half--)
+{
+temp = s[len - i - 1];
+s[len - i - 1] = s[i];
+s[i] = temp;
+i++;
+}
 }
